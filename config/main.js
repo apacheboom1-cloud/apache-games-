825e8a6c074c15d203330e03070eae82326be543
apacheboom1-cloud <apacheboom1@gmail.com>
2026-03-07 22:17:26 -0800

    Update main.js
    uauaoaodiyquiolwd

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "apache games"; // Change this to change the name of your website.
+var subtext = "boom boom"; // set the subtext
 
 // more settings in main.css
 
